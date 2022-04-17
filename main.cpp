49d14d72f5964fc02068f42ed26da4d8d487df8f
Konstantin Kosolapov <kkosolapov@gmail.com>
2022-04-17 19:35:46 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,52 @@
-import Foundation
+#include <iostream>
+#include <string>
+#include <iomanip>
 
-let alphabet = [String]("А","Б","В","Г","Д","Е","Ё","Ж","З","И","Й","К","Л","М","Н","О","П","Р","С","Т","У","Ф","Х","Ц",
-"Ч","Ш","Щ","Ъ","Ы","Ь","Э","Ю","Я")
-let k = 3
+using namespace std;
 
-print()
+string CaesarCipher(int displacement, string &input);
+
+int main(int argc, const char* argv[]){
+  string message;
+  int displace;
+
+  cout << "Please enter a message to be coded." << endl;
+  cin >> message;
+  cout << "Please enter the displacement value between 0 and 25." << endl;
+  cin >> displace;
+
+  if(displace > 25 || displace < 0){
+    cout << "Displacement must be between 0 and 25" << endl;
+  }else{
+    string coded = CaesarCipher(displace, message);
+    cout << message << "\n" << coded << "\nend\n";
+  }
+
+  return 0;
+}
+
+string CaesarCipher(int displacement, string &input){
+  string coded = "";
+
+  for(int i = 0; i < input.length(); i++){
+    char currChar = input[i];
+    if(currChar >= 65 && currChar <= 90){
+      currChar += displacement;
+      if(currChar > 90){
+        currChar -= 26;
+      }
+    }else if(currChar >= 97 && currChar <= 122){
+      if(displacement + currChar >= 128){
+        currChar -= 26;
+      }
+      currChar += displacement;
+      if(currChar > 122){
+        currChar -= 26;
+      }
+    }
+    coded += currChar;
+  }
+
+
+  return coded;
+}
