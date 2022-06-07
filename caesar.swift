107bd47508448dc121a1307690b983c86cfd95a9
Konstantin Kosolapov <kkosolapov@gmail.com>
2022-06-07 07:26:26 +0300

    Update caesar.swift

--- caesar.swift
+++ caesar.swift
@@ -1,33 +1,33 @@
 import Foundation
 
 func CaesarCipher(displacement: Int, input: String) -> String {
-  var coded = "";
+  var coded = ""
 
   for i in 0...input.length(){
     char currChar = input[i];
     if currChar >= 65 && currChar <= 90{
       currChar += displacement;
       if currChar > 90{
-        currChar -= 26;
+        currChar -= 26
       }
     }else if currChar >= 97 && currChar <= 122{
       if displacement + currChar >= 128{
-        currChar -= 26;
+        currChar -= 26
       }
       currChar += displacement;
       if currChar > 122{
-        currChar -= 26;
+        currChar -= 26
       }
     }
-    coded += currChar;
+    coded += currChar
   }
 
-  return coded;
+  return coded
 }
 
 
-var message;
-var displace;
+var message
+var displace
 
 print("Please enter a message to be coded\n")
 message = readline()
@@ -40,5 +40,5 @@ if displace > 25 || displace < 0 {
    print(\(message)
    print("\n")
    print(\(coded))
-   print("\n");
+   print("\n")
 }
